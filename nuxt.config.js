5dfec70863b146c9995ce64392fd100de85a05dc
Ivan <aleksanivan@gmail.com>
2021-12-07 16:14:43 +0300

    added images

--- nuxt.config.js
+++ nuxt.config.js
@@ -81,7 +81,15 @@ export default {
     },
   },
   image: {
-
+    screens: {
+      xs: 320,
+      sm: 450,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xxl: 1536,
+      '2xl': 1536
+    },
   },
   styleResources: {
     scss: [
